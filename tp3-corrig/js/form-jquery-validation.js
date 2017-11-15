af8a0193bc0ae97bd12868bfbe5208be7c2aa164
amazouz92 <32832955+amazouz92@users.noreply.github.com>
2017-11-15 20:00:30 +0100

    Add files via upload

---
+++ tp3-corrig/js/form-jquery-validation.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function () {
+    
+    console.log("DOM ready!");
+    $("button").click(function (event) {
+        event.preventDefault();
+        if ($("#Inputnom").val() == ""
+            || $("#inputprenom").val() == ""
+            || $("#inputdatedenaissance").val() == ""
+            || $("#inputAdresse").val() == ""
+            || $("#InputEmail").val() == "") {
+            alert("veuillez remplir tous les champs")
+        } else {
+            $('#myModal').modal("show");
+            $(".modal-body").html('vous etes '
+            +$("#Inputnom ").val()+' '
+            +$("#inputprenom ").val()
+            +' et vous etes nés le '
+            +$("#inputdatedenaissance").val()
+            +' et vous habitez à '+'<a href=https://www.google.fr/maps/search/ target="_blank"> localisez moi </a>'       
+            +'<img src="map.png" width="330" height="300"/>'+'<br/>'
+            
+            
+        
+        
+        );
+        
+        
+            $('#myModal').modal("show")
+        
+           
+        }
+    });
+});
